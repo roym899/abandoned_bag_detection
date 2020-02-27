fb7fa5d00ace1c41b5acd30b6c723a204de99f03
Ciwan Ceylan <ciwan.ceylan@seb.se>
2020-02-27 21:11:44 +0100

    refactored SimpleTracker and implemented minimal test notebook

--- abandoned_bag_heuristic.py
+++ abandoned_bag_heuristic.py
@@ -8,6 +8,8 @@ PERSON_LABEL = 0
 
 
 def split_bag_persons(centers, labels):
+    assert isinstance(labels, np.ndarray)
+    assert isinstance(centers, np.ndarray)
     bag_bounding_centers = centers[labels == BAG_LABEL]
     persons_bounding_centers = centers[labels == PERSON_LABEL]
 
@@ -30,9 +32,9 @@ def extract_bag_to_ppl_vectors(boudning_boxes, labels):
     centers = compute_center(boudning_boxes)
     bag_centers, persons_centers = split_bag_persons(centers, labels)
     distances = cdist(bag_centers, persons_centers)
-    ind = distances.argrmin(axis=1)
+    ind = distances.argmin(axis=1)
     dist = distances[np.arange(len(ind)), ind]
-    vectors = persons_centers[ind, :] - bag_centers
+    # vectors = persons_centers[ind, :] - bag_centers
 
     return dist, ind
 
@@ -41,57 +43,99 @@ def array_ind_to_key(ordered_dict, ind):
     return ordered_dict.keys()[ind]
 
 
-class SimpleDetector:
+class SimpleTracker:
 
-    def __init__(self, im_height, im_width, self_association_thres, bag_person_thres):
-        self.bag_centers = OrderedDict()
-        self.persons_centers = OrderedDict()
+    def __init__(self, self_association_thres, bag_person_thres):
+        self.all_centers = {'bags': OrderedDict(),
+                            'persons': OrderedDict()}
+        self.prev_frame_centers = {'bags': np.full(shape=(1, 3), fill_value=np.inf),
+                                   'persons': np.full(shape=(1, 3), fill_value=np.inf)}
+        self.prev_frame_ids = {'bags': [],
+                               'persons': []}
+
         self.bag_person_association = dict()
-        self.im_height = im_height
-        self.im_width = im_width
-        self.bag_count = 0
-        self.persons_count = 0
+        self.bag_person_dist = dict()
+        self.instance_count = {'bags': 0,
+                               'persons': 0}
         self.bag_person_thres = bag_person_thres
         self.self_association_thres = self_association_thres
 
-    def bag_dict_to_array(self):
-        return np.array([self.bag_centers[bag_id] for bag_id in self.bag_centers.keys()])
+    def frame2frame_association(self, new_centers, tag):
+        frame_ids = []
+        new_frame_unused_centers = list(range(new_centers.shape[0]))
+        if len(self.prev_frame_ids[tag]) > 0:
+            distances = cdist(self.prev_frame_centers[tag], new_centers)
 
-    def person_dict_to_array(self):
-        return np.array([self.bag_centers[bag_id] for bag_id in self.bag_centers.keys()])
+            new_frame_closest_centers = distances.argmin(axis=1)
+            new_frame_closest_centers_list = new_frame_closest_centers.tolist()
+            new_frame_closest_centers_list.reverse()  # reverse to pop in correct order
+            new_frame_unused_centers = list(set(new_frame_unused_centers) - set(new_frame_closest_centers_list))
+
+            min_dist = distances[range(len(self.prev_frame_ids[tag])), new_frame_closest_centers]
+
+            for dist, prev_frame_id, new_center in zip(min_dist, self.prev_frame_ids[tag],
+                                                       new_centers[new_frame_closest_centers]):
+                association_ind = new_frame_closest_centers_list.pop()
+
+                if dist < self.self_association_thres and association_ind not in new_frame_closest_centers_list:
+                    # case where there is a unique closest center
+                    self.all_centers[tag][prev_frame_id] = new_center
+                    frame_ids.append((association_ind, prev_frame_id))
+                elif dist > self.self_association_thres:
+                    # case where the closest frame is too far away
+                    self.all_centers[tag][self.instance_count[tag]] = new_center
+                    frame_ids.append((association_ind, self.instance_count[tag]))
+                    self.instance_count[tag] += 1
+                else:
+                    # case where one new center is closest to several centers
+                    other_dists = min_dist[new_frame_closest_centers == association_ind]
+                    if dist <= other_dists.min():
+                        self.all_centers[tag][prev_frame_id] = new_center
+                        frame_ids.append((association_ind, prev_frame_id))
+
+        # add the new centers which were not closest to any old center
+        for new_center, association_ind in zip(new_centers[new_frame_unused_centers, :], new_frame_unused_centers):
+            self.all_centers[tag][self.instance_count[tag]] = new_center
+            frame_ids.append((association_ind, self.instance_count[tag]))
+            self.instance_count[tag] += 1
+
+        self.prev_frame_centers[tag] = new_centers
+        print(frame_ids)
+        self.prev_frame_ids[tag] = np.array(sorted(frame_ids, key=lambda x: x[0]))[:, 1]
+
+    def update_bag_person_association(self):
+        for bag_id in self.prev_frame_ids['bags']:
+            if bag_id not in self.bag_person_association or self.bag_person_association[bag_id] is None:
+                person_id, dist = self.find_bag_owner(bag_id)
+                self.bag_person_association[bag_id] = person_id
+                self.bag_person_dist[bag_id] = dist
+            elif self.bag_person_association[bag_id] not in self.prev_frame_ids['persons']:
+                self.bag_person_dist[bag_id] = float('inf')
+            else:
+                bag_person_vector = (self.all_centers['persons'][self.bag_person_association[bag_id]] -
+                                     self.all_centers['bags'][bag_id])
+                self.bag_person_dist[bag_id] = np.sqrt(np.power(bag_person_vector, 2).sum())
+
+    def find_bag_owner(self, bag_id):
+        bag_center = self.all_centers['bags'][bag_id]
+        dists = []
+        for person_id in self.prev_frame_ids['persons']:
+            person_center = self.all_centers['persons'][person_id]
+            dists.append(np.sqrt(np.power(person_center - bag_center, 2).sum()))
+        closest_person_ind = np.array(dists).argmin()
+        if dists[closest_person_ind] < self.bag_person_thres:
+            return self.prev_frame_ids['persons'][closest_person_ind], dists[closest_person_ind]
+        else:
+            return None, float('inf')
 
     def update(self, boxes, labels):
         centers = compute_center(boxes)
         bag_bounding_centers, persons_bounding_centers = split_bag_persons(centers, labels)
 
-        persons_array = self.person_dict_to_array()
+        self.frame2frame_association(persons_bounding_centers, 'persons')
+        self.frame2frame_association(bag_bounding_centers, 'bags')
+        self.update_bag_person_association()
 
-        persons_distances = cdist(persons_bounding_centers, persons_array)
-        persons_association_ind = persons_distances.argmin(axis=1)
-        persons_min_dist = persons_distances[np.arange(len(persons_association_ind)), persons_association_ind]
-        for persons_dist, persons_ind, input_persons_center in zip(persons_min_dist, persons_association_ind,
-                                                                   persons_bounding_centers):
-            if persons_dist > self.self_association_thres:
-                self.persons_centers[self.persons_count] = input_persons_center
-                self.persons_count += 1
-            else:
-                self.persons_centers[list(self.persons_centers.keys())[persons_ind]] = input_persons_center
-
-        persons_array = self.person_dict_to_array()
-        bag_array = self.bag_dict_to_array()
-        bag_distances = cdist(bag_bounding_centers, bag_array)
-        bag_association_ind = bag_distances.argmin(axis=1)
-        bag_min_dist = bag_distances[np.arange(len(bag_association_ind)), bag_association_ind]
-        for bag_dist, bag_ind, input_bag_center in zip(bag_min_dist, bag_association_ind, bag_bounding_centers):
-            if bag_dist > self.self_association_thres:
-                self.bag_centers[self.bag_count] = input_bag_center
-                distances = cdist(input_bag_center, persons_array)
-                closest_person_ind = distances.argmin(axis=1)
-                self.bag_person_association[self.bag_count] = list(self.persons_centers.keys())[closest_person_ind]
-                self.bag_count += 1
-            else:
-                self.bag_centers[list(self.bag_centers.keys())[bag_ind]] = input_bag_center
-
-        bag_array = self.bag_dict_to_array()
-        person_ind = [ind for bag_id, ind in self.bag_person_association.items()]
-        return bag_array, persons_array[person_ind, :], person_ind
+    def is_unattended(self):
+        distances = np.array([self.bag_person_dist[bag_id] for bag_id in self.prev_frame_ids['bags']])
+        return distances > self.bag_person_thres
