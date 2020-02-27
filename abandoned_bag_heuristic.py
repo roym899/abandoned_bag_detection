ad9c7457da509a0a2bbd6956a218c9e0a7204e5e
Ciwan Ceylan <ciwan.ceylan@seb.se>
2020-02-27 12:56:49 +0100

    added distance calc

--- abandoned_bag_heuristic.py
+++ abandoned_bag_heuristic.py
@@ -1,11 +1,11 @@
-
 import numpy as np
+from scipy.spatial.distance import cdist
 
 BAG_LABEL = 28
 PERSON_LABEL = 0
 
-def split_bag_persons(centers, labels):
 
+def split_bag_persons(centers, labels):
     bag_bounding_centers = centers[labels == BAG_LABEL]
     persons_bounding_centers = centers[labels == PERSON_LABEL]
 
@@ -16,9 +16,19 @@ def compute_center(bounding_boxes):
     x_dist = bounding_boxes[:, 2] - bounding_boxes[:, 0]
     y_dist = bounding_boxes[:, 3] - bounding_boxes[:, 1]
     centers = bounding_boxes[:, 0:2] + 0.5 * np.stack((x_dist, y_dist), axis=1)
-    return centers
+    centers_3d = add_z_coordinate(centers)
+    return centers_3d
+
+
+def add_z_coordinate(centers):
+    return np.concatenate((centers, np.zeros(shape=(centers.shape[0], 1))), axis=1)
+
 
 def extract_bag_to_ppl_vectors(boudning_boxes, labels):
-
     centers = compute_center(boudning_boxes)
     bag_centers, persons_centers = split_bag_persons(centers, labels)
+    distances = cdist(bag_centers, persons_centers)
+    dist, ind = distances.min(axis=1)
+    return dist, ind
+
+
