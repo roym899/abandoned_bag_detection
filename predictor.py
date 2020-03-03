adda48e076f65f9c883b0387f49d51c3f7b6b1b1
matteoi <matteoi@kth.se>
2020-03-03 14:58:25 +0100

    saving predictions

--- predictor.py
+++ predictor.py
@@ -13,6 +13,11 @@ from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
+SAVED_PREDICTIONS = []
+
+import pickle
+
+ITER = 0
 
 class VisualizationDemo(object):
     def __init__(self, cfg, instance_mode=ColorMode.IMAGE, parallel=False):
@@ -99,7 +104,14 @@ class VisualizationDemo(object):
             elif "instances" in predictions:
                 predictions = predictions["instances"].to(self.cpu_device)
                 # tracker.update(boxes=predictions.pred_boxes.tensor.numpy(), labels=predictions.pred_classes.numpy())
+                if ITER < 100 :
+                    SAVED_PREDICTIONS.append(predictions)
+                    if ITER == 99 :
+                        with open('predictions.pkl', 'w') as fp:
+                            pickle.dump(SAVED_PREDICTIONS, fp)
+                            print('Saving done!')
                 vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
+                ITER +=1
             elif "sem_seg" in predictions:
                 vis_frame = video_visualizer.draw_sem_seg(
                     frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
