2b10e8a1706445aa0999689a879c7d9135c69919
Leonard Bruns <roym899@gmail.com>
2020-03-13 16:38:12 +0100

    Clean up repo

--- run_default_model.sh
+++
@@ -1,1 +0,0 @@
-python demo.py --webcam 0 --confidence-threshold 0.9 --opts MODEL.WEIGHTS https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl 
