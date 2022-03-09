4c341e109451c1db0879ac87671c76dcd5d27414
Leonard Bruns <roym899@gmail.com>
2022-03-09 13:20:33 +0100

    Fix model link

--- run_trained_finetuned_model.sh
+++ run_trained_finetuned_model.sh
@@ -1,1 +1,1 @@
-python3 demo.py --webcam 0 --confidence-threshold 0.9 --opts MODEL.WEIGHTS https://kth.box.com/shared/static/dzixwunoyan8y1hfer2eutw2j5dqvw9v.pth MODEL.ROI_HEADS.NUM_CLASSES 2 
+python3 demo.py --webcam 0 --confidence-threshold 0.9 --opts MODEL.WEIGHTS https://cdn-123.anonfiles.com/l7GcV4Mcx3/1a422e69-1646828870/model_trained.pth MODEL.ROI_HEADS.NUM_CLASSES 2 
