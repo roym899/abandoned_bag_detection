20bccad03977c3efcefc45300145ecf6948f37ef
Leonard Bruns <leonardb@kth.se>
2022-06-27 20:17:00 +0200

    Move model weights to github release

--- run_trained_finetuned_model.sh
+++ run_trained_finetuned_model.sh
@@ -1,1 +1,1 @@
-python3 demo.py --webcam 0 --confidence-threshold 0.9 --opts MODEL.WEIGHTS https://cdn-123.anonfiles.com/l7GcV4Mcx3/1a422e69-1646828870/model_trained.pth MODEL.ROI_HEADS.NUM_CLASSES 2 
+python3 demo.py --webcam 0 --confidence-threshold 0.9 --opts MODEL.WEIGHTS https://github.com/roym899/abandoned_bag_detection/releases/download/v1.0.0/model_trained.pth MODEL.ROI_HEADS.NUM_CLASSES 2 
